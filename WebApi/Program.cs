98ea2854178b6e6c1ed6559765a0431bba37d448
gustavo fring <gustavofring254@gmail.com>
2025-03-08 17:13:32 +0500

    start of development

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using CoffeeSharp.WebApi.Infrastructure.Data;
+using Microsoft.OpenApi.Models;
 
 namespace WebApi
 {
@@ -12,15 +13,32 @@ namespace WebApi
             builder.Services.AddDbContext<CoffeeSharpDbContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoffeeSharp API", Version = "v1" });
+            });
+
             var app = builder.Build();
 
             using (var scope = app.Services.CreateScope())
             {
-                Thread.Sleep(10000);
                 var dbContext = scope.ServiceProvider.GetRequiredService<CoffeeSharpDbContext>();
                 dbContext.Database.EnsureDeleted();
                 dbContext.Database.EnsureCreated();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoffeeSharp API v1");
+            });
+
+            app.MapGet("/", () => Results.Redirect("/swagger"));
+
+            app.MapControllers();
 
             app.Run();
         }
