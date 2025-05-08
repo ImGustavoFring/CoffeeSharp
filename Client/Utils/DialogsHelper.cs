664917cc94ca372292f6a0e99c88591aa6825257
dail45 <dail45@yandex.ru>
2025-05-09 00:23:55 +0500

    add DialogsHelper to shot Ok and Error

---
+++ Client/Utils/DialogsHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+
+namespace Client.Utils;
+
+public static class DialogsHelper
+{
+    public static async Task ShowOk(string? message = null)
+    {
+        await MessageBoxManager
+            .GetMessageBoxStandard("Успех", !string.IsNullOrEmpty(message) ? message : "Успех!", ButtonEnum.Ok)
+            .ShowAsync();
+    }
+
+    public static async Task ShowError(string? message = null)
+    {
+        await MessageBoxManager
+            .GetMessageBoxStandard("Ошибка", !string.IsNullOrEmpty(message) ? message : "Ошибка!", ButtonEnum.Ok)
+            .ShowAsync();
+    }
+}
