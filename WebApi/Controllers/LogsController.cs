3ca4a3f4716a8233b55701f92368b59563801725
gustavo fring <gustavofring254@gmail.com>
2025-05-11 12:56:26 +0500

    refactor: added filtering by dates, by log level. Moved the total number of records to the headers.

--- WebApi/Controllers/LogsController.cs
+++ WebApi/Controllers/LogsController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Data;
 using Dapper;
 using Domain.DTOs.Log.Responses;
+using Domain.Enums;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -22,32 +22,60 @@ namespace WebApi.Controllers
         [HttpGet]
         public async Task<IActionResult> Get(
             [FromQuery] int pageSize = 100,
-            [FromQuery] int pageIndex = 0)
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] DateTime? from = null,
+            [FromQuery] DateTime? to = null,
+            [FromQuery] LogLevelEnum? level = null)
         {
-            const string sql = @"
-            SELECT 
-                ""level"",
-                ""timestamp"",
-                ""log_event"",
-                ""message"",
-                ""message_template"",
-                ""exception""
-            FROM ""logs""
-            ORDER BY ""timestamp"" DESC
-            LIMIT @Limit OFFSET @Offset";
+            var whereClauses = new List<string>();
+            var parameters = new DynamicParameters();
 
-            var logs = await _db.QueryAsync<LogEntryResponse>(sql, new { Limit = pageSize, Offset = pageIndex });
+            if (from.HasValue)
+            {
+                whereClauses.Add(@"""timestamp"" >= @From");
+                parameters.Add("From", from.Value);
+            }
+
+            if (to.HasValue)
+            {
+                whereClauses.Add(@"""timestamp"" <= @To");
+                parameters.Add("To", to.Value);
+            }
+
+            if (level.HasValue)
+            {
+                parameters.Add("Level", (int)level.Value);
+                whereClauses.Add(@"""level"" = @Level");
+            }
+
+            var whereSql = whereClauses.Count > 0
+                ? "WHERE " + string.Join(" AND ", whereClauses)
+                : "";
+
+            var countSql = $@"SELECT COUNT(*) FROM ""logs"" {whereSql};";
+            var totalCount = await _db.ExecuteScalarAsync<int>(countSql, parameters);
+
+            var dataSql = $@"
+                SELECT
+                    ""level""      AS Level,
+                    ""timestamp""  AS TimeStamp,
+                    ""log_event""  AS LogEvent,
+                    ""message""    AS Message,
+                    ""message_template"" AS MessageTemplate,
+                    ""exception""  AS Exception
+                FROM ""logs""
+                {whereSql}
+                ORDER BY ""timestamp"" DESC
+                LIMIT @Limit OFFSET @Offset;";
+
+            parameters.Add("Limit", pageSize);
+            parameters.Add("Offset", pageIndex * pageSize);
+
+            var logs = await _db.QueryAsync<LogEntryResponse>(dataSql, parameters);
+
+            Response.Headers.Add("X-Total-Count", totalCount.ToString());
+
             return Ok(logs);
-        }
-
-        [HttpGet("count")]
-        public async Task<IActionResult> GetLogsCount()
-        {
-            const string sql = "SELECT COUNT(*) FROM \"logs\"";
-
-            var count = await _db.ExecuteScalarAsync<int>(sql);
-
-            return Ok(count);
         }
     }
 }
